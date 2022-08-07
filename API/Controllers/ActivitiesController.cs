2f6963d95cc93595c860541f0559510db017baa9
Diego <diego.canizales1995@gmail.com>
2022-08-07 16:30:29 -0500

    Walking skeleton API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+	public class ActivitiesController : BaseApiController
+	{
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+		{
+            this._context = context;
+        }
+
+        [HttpGet] //    /activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await this._context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //    /activites/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await this._context.Activities.FindAsync(id);
+        }
+
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        using var scope = host.Services.CreateScope();
+
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedData(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error ocurred during migration");
+        }
+
+        await host.RunAsync();
+    }
+
+    // Dotnet comes with a web server called Kestrel that's going
+    // to run and serve the application on the URL defined through
+    // launchedSettings.json. It will also load configurations
+    // defined in appsettings.json and appsettings.Development.json
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
